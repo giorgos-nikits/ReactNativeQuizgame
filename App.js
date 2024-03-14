a51748b56b61485fed9a3225b71b44988a650070
Bakartu <nikitasgio21@gmail.com>
2024-03-14 19:14:15 +0200

    First commit a quiz game app using react native Bugs: When you get correct answers the score doesn't show.

--- App.js
+++ App.js
@@ -1,11 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [currentQuestion, setCurentQuestion] = useState(0);
+
+  const [score, setScore] = useState(0);
+
+  const [showScore, setShowScore] = useState(false);
+
+  const quizData = [
+    {
+      question: 'What is the capital of India?',
+      options: ['Chandigarh', 'Goa', 'Delhi', 'Shimla'],
+      answer: 'Delhi'
+    },
+    {
+      question: 'Largest animal in the world?',
+      options: ['Elephant', 'Blue whale', 'Hippopotamus', 'iragge'],
+      answer: 'Blue whale'
+    }
+  ]
+
+  const handleAnswer = (selectedAnswer) => {
+    const answer = quizData[currentQuestion]?.answer;
+    if(answer === selectedAnswer){
+      setScore((prevScore)=> prevScore + 1)
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if(nextQuestion < quizData.length){
+      setCurentQuestion(nextQuestion);
+    }else{
+      setShowScore(true);
+    }
+
+  }
+
+  const handleRestart = () => {
+    setCurentQuestion(0);
+    setScore(0);
+    setShowScore(false);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {showScore ? <View>
+        <Text style={styles.optionStyle}>{score}</Text>
+        <TouchableOpacity style={styles.optionContainer}>
+          <Text style={styles.resetButton} onPress={handleRestart}>
+            Reset
+          </Text>
+        </TouchableOpacity>
+      </View> :
+      <View style={styles.questionContainer}>
+        <Text style={styles.Question}>
+          {quizData[currentQuestion]?.question}
+        </Text>
+        {quizData[currentQuestion]?.options.map((item) => {
+          return <TouchableOpacity onPress={() => handleAnswer(item)} style={styles.optionContainer}>
+            <Text style={styles.optionStyle}> {item} </Text>
+          </TouchableOpacity>
+        })}
+
+
+      </View>
+      }
+     
     </View>
   );
 }
@@ -17,4 +79,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  questionContainer:{
+    backgroundColor: '@DDDDDD',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+  },
+  optionStyle:{
+    color: 'green',
+    padding: 5,
+    alignSelf: 'center',
+    fontSize:20,
+  },
+  optionContainer:{
+    borderColor: 'black',
+    borderWidth:2,
+    marginTop:15,
+
+  },
+  Question:{
+    fontSize:24,
+  },
+  resetButton:{
+    fontSize: 18,
+    paddingHorizontal:10,
+  }
 });
